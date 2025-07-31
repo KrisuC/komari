397f45d18ea21aea384a7aeed1d2a9cd19df3085
sasanquaa <arestvn@gmail.com>
2025-07-31 19:19:06 +0700

    test: fix tests

--- backend/src/player/solve_rune.rs
+++ backend/src/player/solve_rune.rs
@@ -477,6 +477,7 @@ mod tests {
             (Rect::default(), KeyKind::F),
         ];
         let mut detector = MockDetector::default();
+        detector.expect_clone();
         detector
             .expect_detect_rune_arrows()
             .return_once(move |_| Ok(ArrowsState::Complete(expected_keys)));
